b8c6d98eab5174d72caeb849e55a78001a864052
alkaran4 <83422829+alkaran4@users.noreply.github.com>
2021-06-30 21:43:13 +0300

    5е ДЗ

---
+++ 3.py
@@ -0,0 +1,23 @@
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+
+def gen_tutors_klasses(list_1, list_2):
+    for i in range(len(list_1)):
+        yield list_1[i], list_2[i]
+
+g = gen_tutors_klasses(tutors, klasses)
+
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+
